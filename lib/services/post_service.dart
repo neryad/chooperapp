95ab13fd14337e3b47dca62e23158081d6b34a45
neryad <dayern.gomez@hotmail.com>
2020-08-29 21:02:27 -0400

    basic ui and implemtations

---
+++ lib/services/post_service.dart
@@ -0,0 +1,29 @@
+import 'package:chopper/chopper.dart';
+import 'package:chooperapp/config/api_config.dart' as conf;
+part 'post_service.chopper.dart';
+
+//final String ApiUrl = conf.CONFIG['API_URL'];
+@ChopperApi(baseUrl: '/posts')
+abstract class PostService extends ChopperService {
+  @Get()
+  Future<Response> getPosts();
+
+  @Get(path: '/{id}')
+  Future<Response> getPost(@Path('id') int id);
+  @Post()
+  Future<Response> postPost(
+    @Body() Map<String, dynamic> body,
+  );
+
+  static PostService create() {
+    final client = ChopperClient(
+      baseUrl: 'https://jsonplaceholder.typicode.com',
+      services: [
+        _$PostService(),
+      ],
+      converter: JsonConverter(),
+    );
+
+    return _$PostService(client);
+  }
+}
